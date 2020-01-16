838282909c65350e2673929a9d394f41e20b1649
qige <574857804@qq.com>
2020-01-16 18:02:30 +0800

    nuxt.js项目框架搭建

---
+++ components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header-wrapper">
+    <el-row type="flex"
+            justify="space-between"></el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+</style>
